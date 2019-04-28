264575df7fdcc8cbe4b60a746672a98d8edee8fc
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2019-04-28 22:38:15 +0100

    Adds logic for add and delete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todo';
+import AddTodo from './AddTodo';
+//import './App.css';
 
 class App extends Component {
+  state = {
+    todos:[
+      {id: 1, content: 'Create ToDo App'},
+      {id: 2, content: 'Demo'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos  = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+    
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h2> todos</h2>
-    
-          
-        </header>
+      <div className="todo-App container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
