17a4ea5524f23f0dc76c5f43f4528cef38d342ed
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2019-05-21 15:40:29 +0100

    adds css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     todos:[
       {id: 1, content: 'Create ToDo App'},
       {id: 2, content: 'Demo'}
-    ]
+    ],
+    isEditing: false
   }
   deleteTodo = (id) => {
     const todos  = this.state.todos.filter(todo => {
@@ -17,8 +18,20 @@ class App extends Component {
     this.setState({
       todos
     })
-    
-  }
+    }
+
+    editTodo = (id) => {
+      this.setState.isEditing = true;
+      console.log(this.state.isEditing);
+      const todos  = this.state.todos.filter(todo => {
+        return todo.id !== id
+     });
+      this.setState({
+        todos
+      })
+      }
+
+
   addTodo = (todo) => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
@@ -27,10 +40,12 @@ class App extends Component {
     })
   }
   render() {
+    //const {isEditing} = this.state;
     return (
       <div className="todo-App container">
         <h1 className="center blue-text">Todo List</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} editToDo={this.editTodo} isEditing={this.state}/>
         <AddTodo addTodo={this.addTodo}/>
       </div>
     );
--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
-const Todos = ({todos, deleteTodo}) => {
-
+const Todos = ({todos, deleteTodo, editTodo, isEditing}) => {
+    //const {isEditing} = this.state;
+    console.log(isEditing)
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
+                
                 <div className="collection-item highlight" key={todo.id}>
-                <span className="striped highlight">{todo.content}</span><button className=" right rcard-panel red lighten-4 " onClick={() => {deleteTodo(todo.id)}}>X</button>
+                <section>
+                    { !isEditing ? <form>
+            <input  type="text" defaultValue={todo.content}/>
+            <button  type="submit">Edit Item</button>
+          </form> : <form><span className="striped highlight">{todo.content}</span>
+                <button className=" right rcard-panel green lighten-4 " onClick={() => {editTodo(todo.id)}}>Edit</button>
+                <button className=" right rcard-panel red lighten-4 " onClick={() => {deleteTodo(todo.id)}}>Del</button>
+                </form> }</section>
+         {/* <form>
+            <input type="text" defaultValue={todo.content}/>
+            <button type="submit">Edit Item</button>
+          </form>
+    
+                <span className="striped highlight">{todo.content}</span>
+                <button className=" right rcard-panel green lighten-4 " onClick={() => {editTodo(todo.id)}}>Edit</button>
+                <button className=" right rcard-panel red lighten-4 " onClick={() => {deleteTodo(todo.id)}}>Del</button>
+         */}
                 </div>
             )
         })
