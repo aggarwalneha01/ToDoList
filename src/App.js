1bd47bb3753f5cc59df01653aab2e6ce2a4a6f2b
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2019-06-05 15:51:01 +0100

    add logic for editing

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
-import React, { Component } from 'react';
-import Todos from './Todo';
-import AddTodo from './AddTodo';
+import React, { Component } from "react";
+import Todos from "./Todo";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
-    todos:[
-      {id: 1, content: 'Create ToDo App'},
-      {id: 2, content: 'Demo'}
-    ],
+    todos: [{ id: 1, content: "Create ToDo App" }, { id: 2, content: "Demo" }],
     isEditing: false
-  }
-  deleteTodo = (id) => {
-    const todos  = this.state.todos.filter(todo => {
-      return todo.id !== id
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
     });
     this.setState({
       todos
-    })
-    }
+    });
+  };
 
-    editTodo = (id) => {
-        this.setState({
-        isEditing: true
-      });
-      console.log(this.state.isEditing);
-      const todos  = this.state.todos.filter(todo => {
-        return todo.id !== id
-     });
-      this.setState({
-        todos
-      })
-    }
+  editTodo = id => {
+    this.setState({
+      isEditing: true
+    });
+  };
 
+  switchToListMode = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
 
-  addTodo = (todo) => {
+  updateTodo = (idToUpdate, newValue) => {
+    const newTodoList = this.state.todos.map(todo => todo.id === idToUpdate ? { id: todo.id, content: newValue } : todo);
+
+    this.setState({
+      todos: newTodoList
+    });
+  };
+
+  addTodo = todo => {
     todo.id = Math.random();
     this.setState(prevState => ({
       todos: [...prevState.todos, todo]
-    }))
-  }
+    }));
+  };
   render() {
     return (
       <div className="todo-App container">
         <h1 className="center blue-text">Todo List</h1>
-        
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} editToDo={this.editTodo} isEditing={this.state.isEditing}/>
-        <AddTodo addTodo={this.addTodo}/>
+
+        <Todos
+          updateTodo={this.updateTodo}
+          todos={this.state.todos}
+          deleteTodo={this.deleteTodo}
+          editToDo={this.editTodo}
+          isEditing={this.state.isEditing}
+          switchToListMode={this.switchToListMode}
+        />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
--- src/Todo.js
+++ src/Todo.js
@@ -1,29 +1,29 @@
 import React from 'react'
 
-const Todos = ({todos, deleteTodo, editToDo, isEditing}) => {
+const Todos = ({ todos, deleteTodo, editToDo, updateTodo, switchToListMode, isEditing }) => {
     console.log(isEditing)
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
-                
+
                 <div className="collection-item highlight" key={todo.id}>
-                <section>
-                    { isEditing ? <form>
-            <input  type="text" defaultValue={todo.content}/>
-            <button  type="submit">Edit Item</button>
-          </form> : <form><span className="striped highlight">{todo.content}</span>
-                <button className=" right rcard-panel green lighten-4 " onClick={(e) => { e.preventDefault(); editToDo(todo.id)}}>Edit</button>
-                <button className=" right rcard-panel red lighten-4 " onClick={() => {deleteTodo(todo.id)}}>Del</button>
-                </form> }</section>
+                    <section>
+                        {isEditing ? <div>
+                            <input type="text" defaultValue={todo.content} onChange={(e) => { updateTodo(todo.id, e.target.value)}}  />
+                            <button onClick={switchToListMode}>List View</button>
+                        </div> : <form><span className="striped highlight">{todo.content}</span>
+                                <button className=" right rcard-panel green lighten-4 " onClick={(e) => { e.preventDefault(); editToDo(todo.id) }}>Edit</button>
+                                <button className=" right rcard-panel red lighten-4 " onClick={() => { deleteTodo(todo.id) }}>Del</button>
+                            </form>}</section>
                 </div>
             )
         })
-    ): (
-        <p className="center">You have nothing left todo!</p>
-    )
+    ) : (
+            <p className="center">You have nothing left todo!</p>
+        )
     return (
         <div className="todos collection">
-        {todoList}
+            {todoList}
         </div>
     )
 }
